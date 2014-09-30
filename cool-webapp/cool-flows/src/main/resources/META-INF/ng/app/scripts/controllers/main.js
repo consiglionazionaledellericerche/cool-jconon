c8f969331f27dc3c7a3c693a2aec0f16aeefe372
Francesco Uliana <francesco.uliana@cnr.it>
2014-09-30 15:28:50 +0000

    prova proxy
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@5606 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-webapp/cool-flows/src/main/resources/META-INF/ng/app/scripts/controllers/main.js
+++ cool-webapp/cool-flows/src/main/resources/META-INF/ng/app/scripts/controllers/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('flowsApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http, $location) {
+
+    $http.get('/cool-flows/rest/common').success(function (data) {
+      $scope.common = data.User;
+    });
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
