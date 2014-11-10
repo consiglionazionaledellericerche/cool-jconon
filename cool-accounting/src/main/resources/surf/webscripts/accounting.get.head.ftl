1dd8c4616dd8fc576ecf59a071c3e364be1a3ad0
Marco Spasiano <marco.spasiano@cnr.it>
2014-11-10 13:19:45 +0000

    Nuova funzionalità per i gestori dei bandi
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@5817 18059cec-d5c9-4679-bb3d-60c028e053f1

---
+++ cool-accounting/src/main/resources/surf/webscripts/accounting.get.head.ftl
@@ -0,0 +1,1 @@
+<script src="${url.context}/res/js/thirdparty/require.js" data-main="ws/accounting/accounting.get.js"></script>
---
+++ cool-webapp/cool-jconon/src/main/resources/META-INF/js/cnr/cnr.ui.gestorisedi.js
@@ -0,0 +1,47 @@
+/*global params*/
+define(['jquery', 'cnr/cnr.url', 'cnr/cnr.ui.select', 'cnr/cnr', 'json!common'], function ($, URL, Select, CNR, common) {
+
+  "use strict";
+
+  function render(data, obj) {
+    var select = obj.setOptions(data);
+    select.on('change', function (event, init) {
+      $.map([].concat(select.data('select2').data()), function (el) {
+        var riga = $.map(data, function(element) {
+          if (el !== null && element.key === el.id) {
+            return element;
+          }
+        }),
+          struttura_destinataria = select.parents('form').find('#struttura_destinataria'),
+          sede = select.parents('form').find('#sede');
+        if (riga.length > 0) {
+          struttura_destinataria.val(riga[0].citta);
+          sede.val(riga[0].descrizione);
+        } else {
+          if (!init) {
+            struttura_destinataria.val('');
+            sede.val('');
+          }
+        }
+      });
+    });
+    select.trigger("change", true);
+  }
+  function widget(id, labelText, item) {
+    var obj = Select.CustomWidget(id, labelText, item),
+      callType = params['call-type'],
+      sediAbilitate = common['managers-call'][callType];
+    if (sediAbilitate && sediAbilitate.length > 0) {
+      render(sediAbilitate, obj);
+    } else {
+      URL.Data.sedi().done(function (data) {
+        render(data.results, obj);
+      });
+    }
+    return obj.emptyWidget;
+  }
+
+  return {
+    Widget: widget
+  };
+});
