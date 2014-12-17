5fe95883c50bff47bb260f659522d73b93028d43
Francesco Uliana <francesco.uliana@cnr.it>
2014-12-17 08:56:15 +0000

    direttiva task-list
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6090 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-webapp/cool-flows/src/main/resources/META-INF/ng/app/scripts/controllers/main.js
+++ cool-webapp/cool-flows/src/main/resources/META-INF/ng/app/scripts/controllers/main.js
@@ -222,22 +222,10 @@ angular.module('flowsApp')
       });
 
 
-      $scope.pooled = [];
-      $scope.takeTask = function (id, user) {
-
-        dataService.proxy.api.taskInstances(null, id, {'cm_owner': user || null}).success(function (data) {
-          console.log(data);
-          $scope.pooled[id] = user !== undefined;
-        });
-      };
-
 
     });
 
-    $scope.modalTaskDiagram = function (task) {
-      var url = dataService.urls.proxy + 'service/api/workflow-instances/' + task.workflowInstance.id + '/diagram';
-      modalService.simpleModal(task.description, url);
-    };
+
 
     $scope.getLabel = function (values, value) {
       return _.filter(values, function (item) {
