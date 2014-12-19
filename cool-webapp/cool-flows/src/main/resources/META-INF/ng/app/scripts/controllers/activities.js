c3dd31e45bd2b7e172df49a977745350b2a8f834
Francesco Uliana <francesco.uliana@cnr.it>
2014-12-19 10:58:28 +0000

    fix activities
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6110 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-webapp/cool-flows/src/main/resources/META-INF/ng/app/scripts/controllers/activities.js
+++ cool-webapp/cool-flows/src/main/resources/META-INF/ng/app/scripts/controllers/activities.js
@@ -10,13 +10,7 @@ angular.module('flowsApp')
         pooledTasks: false
       })
       .success(function (tasks) {
-
-        var filteredTasks = tasks.data;
-
-        $scope.tasks = _.groupBy(filteredTasks, function (el) {
-          return el.workflowInstance.title;
-        });
-
+         $scope.tasks = tasks.data;
       });
 
   });
