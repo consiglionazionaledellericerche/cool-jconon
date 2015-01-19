785da223f0c1c250a1c7d44b8ac4f63e9ab001e7
Francesco Uliana <francesco.uliana@cnr.it>
2015-01-19 16:18:04 +0000

    session filter REST
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6166 18059cec-d5c9-4679-bb3d-60c028e053f1

---
+++ cool-webapp/cool-flows/src/main/java/it/cnr/flows/rest/SessionFilter.java
@@ -0,0 +1,36 @@
+package it.cnr.flows.rest;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+public class SessionFilter implements ContainerRequestFilter {
+
+    @Context
+    HttpServletRequest request;
+
+    @Override
+    public void filter(ContainerRequestContext requestContext)
+            throws IOException {
+
+        HttpSession session = request.getSession(false);
+
+        if (session == null && !isPublicUrl(request.getRequestURI())) {
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("Session exipred.").build());
+        }
+
+    }
+
+    private boolean isPublicUrl (String url) {
+
+        return url.indexOf("/rest/static") >  0 || url.indexOf("rest/security/login") > 0;
+
+    }
+}jV
