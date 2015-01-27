4d24c540cdb66943e158af67f015fa9612ec4225
Francesco Uliana <francesco.uliana@cnr.it>
2015-01-27 10:10:03 +0000

    integrazione hazelcast in doccnr
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6223 18059cec-d5c9-4679-bb3d-60c028e053f1

---
+++ cool-webapp/cool-doccnr/src/main/java/it/cnr/doccnr/config/CacheConfiguration.java
@@ -0,0 +1,120 @@
+package it.cnr.doccnr.config;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.instance.HazelcastInstanceFactory;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MaxSizeConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(CacheConfiguration.class);
+
+    private static HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+
+    @Inject
+    private Environment env;
+
+    private CacheManager cacheManager;
+
+    @PreDestroy
+    public void destroy() {
+        log.info("Closing Cache Manager");
+        Hazelcast.shutdownAll();
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        log.debug("Starting HazelcastCacheManager");
+        cacheManager = new com.hazelcast.spring.cache.HazelcastCacheManager(hazelcastInstance);
+        return cacheManager;
+    }
+
+    @PostConstruct
+    private HazelcastInstance hazelcastInstance() {
+        final Config config = new Config();
+        config.setInstanceName("workers");
+        config.getNetworkConfig().setPort(5701);
+        config.getNetworkConfig().setPortAutoIncrement(true);
+
+
+          config.getNetworkConfig().getJoin().getAwsConfig().setEnabled(false);
+          config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(true);
+          config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(false);
+
+        config.getMapConfigs().put("default", initializeDefaultMapConfig());
+        config.getMapConfigs().put("it.cnr.si.dl.jhipster.domain.*", initializeDomainMapConfig());
+
+        hazelcastInstance = HazelcastInstanceFactory.newHazelcastInstance(config);
+
+        return hazelcastInstance;
+    }
+
+    private MapConfig initializeDefaultMapConfig() {
+        MapConfig mapConfig = new MapConfig();
+
+        /*
+            Number of backups. If 1 is set as the backup-count for example,
+            then all entries of the map will be copied to another JVM for
+            fail-safety. Valid numbers are 0 (no backup), 1, 2, 3.
+         */
+        mapConfig.setBackupCount(0);
+
+        /*
+            Valid values are:
+            NONE (no eviction),
+            LRU (Least Recently Used),
+            LFU (Least Frequently Used).
+            NONE is the default.
+         */
+        mapConfig.setEvictionPolicy(MapConfig.EvictionPolicy.LRU);
+
+        /*
+            Maximum size of the map. When max size is reached,
+            map is evicted based on the policy defined.
+            Any integer between 0 and Integer.MAX_VALUE. 0 means
+            Integer.MAX_VALUE. Default is 0.
+         */
+        mapConfig.setMaxSizeConfig(new MaxSizeConfig(0, MaxSizeConfig.MaxSizePolicy.USED_HEAP_SIZE));
+
+        /*
+            When max. size is reached, specified percentage of
+            the map will be evicted. Any integer between 0 and 100.
+            If 25 is set for example, 25% of the entries will
+            get evicted.
+         */
+        mapConfig.setEvictionPercentage(25);
+
+        return mapConfig;
+    }
+
+    private MapConfig initializeDomainMapConfig() {
+        MapConfig mapConfig = new MapConfig();
+
+        mapConfig.setTimeToLiveSeconds(env.getProperty("cache.timeToLiveSeconds", Integer.class, 3600));
+        return mapConfig;
+    }
+
+
+    /**
+    * @return the unique instance.
+    */
+    public static HazelcastInstance getHazelcastInstance() {
+        return hazelcastInstance;
+    }
+}
