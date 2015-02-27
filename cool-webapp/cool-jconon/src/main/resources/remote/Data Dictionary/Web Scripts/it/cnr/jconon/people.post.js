4d7e7c1c71510cca75c049496cc36407c9ed3ac2
Francesco Uliana <francesco.uliana@cnr.it>
2015-02-27 09:58:33 +0000

    refactoring export people
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6433 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-webapp/cool-jconon/src/main/resources/remote/Data Dictionary/Web Scripts/it/cnr/jconon/people.post.js
+++ cool-webapp/cool-jconon/src/main/resources/remote/Data Dictionary/Web Scripts/it/cnr/jconon/people.post.js
@@ -1,138 +1,143 @@
 /*global cnrutils,logger,requestbody,jsonUtils */
 
-function main(nodeRef, mapping) {
-
+(function () {
   "use strict";
-  var d = cnrutils.getBean('dictionaryService'),
-    application = search.findNode(nodeRef),
-    rows = application.children,
-    out = [],
-    i,
-    row,
-    type,
-    ps,
-    p,
-    value,
-    k,
-    item;
+
+  var d = cnrutils.getBean('dictionaryService');
+
+  var defaultMapping = {
+    version: '1.2',
+    props: {
+      "{http://www.cnr.it/model/cvelement/1.0}ruoloIncarico": "ruolo",
+      "{http://www.cnr.it/model/cvelement/1.0}attivitainCorso": "attivita_incorso",
+      "{http://www.cnr.it/model/cvelement/1.0}periodAttivitaDal": "attivita_dal",
+      "{http://www.cnr.it/model/cvelement/1.0}periodAttivitaAl": "attivita_al",
+      "{http://www.cnr.it/model/cvelement/1.0}attivitaSvolta": "descr",
+      "{http://www.cnr.it/model/cvelement/1.0}dettagli": "descr",
+      "{http://www.cnr.it/model/cvelement/1.0}:titoloProgetto": "titolo",
+      "{http://www.cnr.it/model/cvelement/1.0}progressivoOrdinamento": false
+    },
+    types: {
+      "{http://www.cnr.it/model/cvelement/1.0}commissione": "gruppo_lavoro",
+      "{http://www.cnr.it/model/cvelement/1.0}riconoscimento": "premi"
+    },
+    mapper: {
+      "{http://www.cnr.it/model/cvelement/1.0}ruoloProgetto": {
+        regex: "_",
+        replacement: " "
+      }
+    }
+  };
 
 
-  function propertyTitle(s) {
+  function getProperty(s) {
     var q = cnrutils.executeStatic('org.alfresco.service.namespace.QName.createQName', s);
     return d.getProperty(q);
   }
 
 
-  function typeTitle(s) {
+  function getType(s) {
     var q = cnrutils.executeStatic('org.alfresco.service.namespace.QName.createQName', s);
     return d.getType(q);
   }
 
-  function isBlacklist(p) {
-    return p.toString() === '{http://www.cnr.it/model/cvelement/1.0}progressivoOrdinamento';
-  }
 
-  function propertyMapper(a, b) {
+  function main(nodeRef, mapping) {
 
-    if (p.toString()1 === '{http://www.cnr.it/model/cvelement/1.0}ruoloProgetto') {
-      return b.replace(/_/g, ' ');
-    } else {
-      return b;
-    }
-
-  }
-
-
-  for (i = 0; i < rows.length; i++) {
-
-    row = rows[i];
-    type = row.type;
-
-    if (type.indexOf('cvelement') < 0) {
-      logger.info('escludo ' + type);
-    } else {
-
-      ps = {
-        altre_info: [],
-        descr: []
-      };
+    var application = search.findNode(nodeRef),
+      rows = application.children,
+      out = [],
+      i,
+      row,
+      type,
+      ps,
+      p,
+      value,
+      k,
+      item;
 
 
-      for (p in row.properties) {
-        if (row.properties.hasOwnProperty(p)) {
-          value = row.properties[p];
-          k = mapping.props[p];
+    for (i = 0; i < rows.length; i++) {
 
+      row = rows[i];
+      type = row.type;
 
-          item = {
-            valore: propertyMapper(p, value),
-            type: propertyTitle(p).dataType.javaClassName,
-            codice_selonline: p,
-            label_selonline: propertyTitle(p).title
-          };
+      if (type.indexOf('cvelement') < 0) {
+        logger.info('escludo ' + type);
+      } else {
 
-          if (k) {
-            if (k === 'descr') {
-              ps[k].push(item);
+        ps = {
+          altre_info: [],
+          descr: []
+        };
+
+        for (p in row.properties) {
+          if (row.properties.hasOwnProperty(p)) {
+            value = row.properties[p];
+            k = mapping.props[p];
+
+            var myProp = getProperty(p);
+
+            var K = mapping.mapper[p];
+            var valore = K ? value.replace(new RegExp(K.regex, "g"), K.replacement) : value;
+
+            item = {
+              valore: valore,
+              type: myProp.dataType.javaClassName,
+              codice_selonline: p,
+              label_selonline: myProp.title
+            };
+
+            if (k) {
+              if (k === 'descr') {
+                ps[k].push(item);
+              } else {
+                ps[k] = item;
+
+              }
             } else {
-              ps[k] = item;
+
+              if (p.indexOf('cvelement') < 0 || mapping.props[p] === false) {
+                logger.info("--- escludo property " + p);
+              } else {
+                ps.altre_info.push(item);
+              }
 
             }
-          } else {
-
-            if (p.indexOf('cvelement') < 0 || isBlacklist(p)) {
-              logger.info("--- escludo property " + p);
-            } else {
-              ps.altre_info.push(item);
-            }
-
           }
+
         }
+
+        out.push({
+          tipo_attivita: {
+            codice_selonline: type,
+            valore: mapping.types[type] || null,
+            label_selonline: getType(type).title
+          },
+          properties: ps
+        });
 
       }
 
-      out.push({
-        tipo_attivita: {
-          codice_selonline: type,
-          valore: mapping.types[type] || null,
-          label_selonline: typeTitle(type).title
-        },
-        properties: ps
-      });
-
     }
 
+    return {
+      rows: out,
+      'mapping-version': mapping.version
+    };
+
   }
 
-  return {
-    rows: out,
-    'mapping-version': mapping.version
-  };
+  var json = jsonUtils.toObject(requestbody.content);
 
-}
+  var mapping = json.mapping || defaultMapping;
 
-var json = jsonUtils.toObject(requestbody.content);
+  var result = main(json.nodeRef, mapping);
+
+  model.json = jsonUtils.toJSONString(result);
 
 
-var defaultMapping = {
-  version: '1.2',
-  props: {
-    "{http://www.cnr.it/model/cvelement/1.0}ruoloIncarico": "ruolo",
-    "{http://www.cnr.it/model/cvelement/1.0}attivitainCorso": "attivita_incorso",
-    "{http://www.cnr.it/model/cvelement/1.0}periodAttivitaDal": "attivita_dal",
-    "{http://www.cnr.it/model/cvelement/1.0}periodAttivitaAl": "attivita_al",
-    "{http://www.cnr.it/model/cvelement/1.0}attivitaSvolta": "descr",
-    "{http://www.cnr.it/model/cvelement/1.0}dettagli": "descr",
-    "{http://www.cnr.it/model/cvelement/1.0}:titoloProgetto": "titolo"
-  },
-  types: {
-    "{http://www.cnr.it/model/cvelement/1.0}commissione": "gruppo_lavoro",
-    "{http://www.cnr.it/model/cvelement/1.0}riconoscimento": "premi"
-  }
-};
+}());
 
-var mapping = json.mapping || defaultMapping;
-
-model.json = jsonUtils.toJSONString(main(json.nodeRef, mapping));
 
 // curl -ujconon:jcononpw -X POST http://as1dock.si.cnr.it:8080/alfresco/service/cnr/jconon/manage-application/people-cvelements -H'Content-type: application/json' --data '{"nodeRef":"workspace://SpacesStore/4c7b03f1-e0be-4112-b2b8-e425980b2b1d", "mapping":{"version":"2.5", "props":{"foo":"bar"}}}' | jq '[.]'
