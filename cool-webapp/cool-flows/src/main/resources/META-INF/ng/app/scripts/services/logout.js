a6e2cfed5c621657bd7e8f7dbe89c7eb1f24d17a
Francesco Uliana <francesco.uliana@cnr.it>
2015-02-27 13:39:17 +0000

    fix minori flows
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6443 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-webapp/cool-flows/src/main/resources/META-INF/ng/app/scripts/services/logout.js
+++ cool-webapp/cool-flows/src/main/resources/META-INF/ng/app/scripts/services/logout.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('flowsApp')
-  .factory('logoutService', function ($location, $sessionStorage, $rootScope) {
+  .factory('logoutService', function ($location, $sessionStorage, $rootScope, $cookies) {
 
     return {
       logout: function () {
         delete $sessionStorage.user;
         delete $sessionStorage.ticket;
+        $cookies.ticket = null;
         $rootScope.user = null;
         $location.path('/login');
       }
