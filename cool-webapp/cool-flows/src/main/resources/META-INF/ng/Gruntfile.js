ab3a2f4ea26821e862fe8a8660a6de7a4590047a
Francesco Uliana <francesco.uliana@cnr.it>
2015-04-13 11:56:18 +0000

    eliminato karma
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6764 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-webapp/cool-flows/src/main/resources/META-INF/ng/Gruntfile.js
+++ cool-webapp/cool-flows/src/main/resources/META-INF/ng/Gruntfile.js
@@ -434,8 +434,7 @@ module.exports = function (grunt) {
     'wiredep',
     'concurrent:test',
     'autoprefixer',
-    'connect:test',
-    'karma'
+    'connect:test'
   ]);
 
   grunt.registerTask('build', [
