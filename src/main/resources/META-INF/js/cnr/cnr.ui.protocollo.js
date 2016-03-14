eeb19406de375bd1ec05d41014c8eb2b61d57cff
Marco Spasiano <marco.spasiano@cnr.it>
2016-03-14 14:11:28 +0100

    Aggiunto nuovo widget Aggiunto il campo numero Protocollo e data Protocollo per tutti gli allegati che sono dei provvedimenti, decreti, bandi ecc. Il campo numero protocollo nonostante sia realmente un numero valuta se sia più corretto farlo inserire come TESTO lunghezza 7, poiché è generalmente espresso con lo/gli zero davanti. Nel caso di campo numerico il prot. 0000125 diventerebbe 125 e sarebbe molto difficile la forzatura per farlo inserire unicamente con lunghezza 7, oppure non so se i può fare con formato guidato come per il CAP. Per i seguenti allegati (tutti gli altri), si può prevedere il campo ma non deve essere obbligatorio l’inserimento (poiché non sono direttamente protocollati ma fanno riferimento ad un documento protocollato) English Call Allegato generico al Bando di concorso re-advertisement revised application form - re-advertisement Testo coordinato

---
+++ src/main/resources/META-INF/js/cnr/cnr.ui.protocollo.js
@@ -0,0 +1,43 @@
+define(['jquery', 'cnr/cnr'], function ($, URL) {
+  "use strict";
+  function strPad(i,l,s) {
+    var o = i.toString();
+    if (!s) { s = '0'; }
+    while (o.length < l) {
+      o = s + o;
+    }
+    return o;
+  };
+  function widget(id, labelText, bulkitem) {
+
+    var input = $('<input type="text" class="input-medium" data-provide="typeahead" />').addClass(bulkitem['class']).attr('id', id).attr('name', id),
+      controls = $('<div class="controls"></div>').append(input),
+      label = $('<label class="control-label"></label>').attr('for', id).text(labelText || ''),
+      item = $('<div class="control-group widget"></div>');
+
+    item
+      .data('id', id)
+      .append(label)
+      .append(controls);
+    if (bulkitem.val)
+      input.val(bulkitem.val);
+    input.keypress(function (e) {
+     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57) || input.val().length >= 7) {
+       return false;
+      }
+    });    
+    input.on('change', function () {
+      if (input.val()) {
+        input.val(strPad(input.val(), 7));
+        item.data('value', input.val());
+      } else {
+        item.data('value', null);
+      }
+    });
+    return item;
+  }
+
+  return {
+    Widget: widget
+  };
+});
