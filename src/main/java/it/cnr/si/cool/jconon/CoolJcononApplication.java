af12e46de86ac74a205f37b1b7628d8975c127ee
Francesco Uliana <francesco.uliana@cnr.it>
2016-05-06 15:31:34 +0200

    urlrewrite

--- src/main/java/it/cnr/si/cool/jconon/CoolJcononApplication.java
+++ src/main/java/it/cnr/si/cool/jconon/CoolJcononApplication.java
@@ -2,12 +2,21 @@ package it.cnr.si.cool.jconon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
 
 @SpringBootApplication
 @ImportResource({"classpath:/META-INF/spring/cool-jconon-context.xml"})
 
 public class CoolJcononApplication {
+
+	@Bean
+	public UrlRewriteFilter getUrlRewriteFilter() {
+		UrlRewriteFilter urlRewriteFilter = new UrlRewriteFilter();
+		return urlRewriteFilter;
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoolJcononApplication.class, args);
