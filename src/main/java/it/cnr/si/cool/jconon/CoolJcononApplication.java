5978aba1ba8c4dccc1f9d9766e0a072e7501a0e4
Francesco Uliana <francesco.uliana@cnr.it>
2016-05-09 14:24:54 +0200

    disabilitato FreeMarkerAutoConfiguration

--- src/main/java/it/cnr/si/cool/jconon/CoolJcononApplication.java
+++ src/main/java/it/cnr/si/cool/jconon/CoolJcononApplication.java
@@ -2,13 +2,13 @@ package it.cnr.si.cool.jconon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = FreeMarkerAutoConfiguration.class)
 @ImportResource({"classpath:/META-INF/spring/cool-jconon-context.xml"})
-
 public class CoolJcononApplication {
 
 	@Bean
