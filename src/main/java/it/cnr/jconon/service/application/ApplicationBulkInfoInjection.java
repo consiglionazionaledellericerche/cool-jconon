2e3acf269bba635d69010c4798e55387c5091d8d
Marco Spasiano <marco.spasiano@cnr.it>
2016-07-19 19:50:37 +0200

    Aggiornamento versione cool, risolto BUG cache sui BulkInfo

--- src/main/java/it/cnr/jconon/service/application/ApplicationBulkInfoInjection.java
+++ src/main/java/it/cnr/jconon/service/application/ApplicationBulkInfoInjection.java
@@ -1,23 +1,19 @@
 package it.cnr.jconon.service.application;
 
+import it.cnr.bulkinfo.BulkInfo;
+import it.cnr.cool.service.BulkInfoInjection;
+import it.cnr.jconon.repository.ApplicationBulkInfoCache;
+
 import java.util.List;
 
-import org.apache.chemistry.opencmis.client.api.ObjectType;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import it.cnr.bulkinfo.BulkInfo;
-import it.cnr.cool.cmis.service.CMISService;
-import it.cnr.cool.service.BulkInfoCoolService;
-import it.cnr.cool.service.BulkInfoInjection;
 
 public class ApplicationBulkInfoInjection implements BulkInfoInjection {
 	
 	private String parentAspect;
 	private List<String> bulkInfos;	
 	@Autowired
-	private CMISService cmisService;
-	@Autowired
-	private BulkInfoCoolService bulkInfoService;	
+	private ApplicationBulkInfoCache applicationBulkInfoCache;
 
 	public void setParentAspect(String parentAspect) {
 		this.parentAspect = parentAspect;
@@ -29,17 +25,9 @@ public class ApplicationBulkInfoInjection implements BulkInfoInjection {
 
 	@Override
 	public void complete(BulkInfo bulkInfo) {
-		ObjectType objectType = cmisService.createAdminSession().getTypeDefinition(parentAspect);
-		completeWithChildren(objectType, bulkInfo);
-		for (String bulkInfoAspect : bulkInfos) {
+		List<String>  aspects = applicationBulkInfoCache.getAspects(bulkInfo.getId(), parentAspect, bulkInfos);
+		for (String bulkInfoAspect : aspects) {
 			bulkInfo.getCmisImplementsName().put(bulkInfoAspect, false);			
-		}
-	}
-
-	private void completeWithChildren(ObjectType objectType, BulkInfo bulkInfo) {
-		for (ObjectType child : objectType.getChildren()) {
-			bulkInfo.getCmisImplementsName().put(child.getId(), false);
-			completeWithChildren(child, bulkInfo);
 		}
 	}
 }
