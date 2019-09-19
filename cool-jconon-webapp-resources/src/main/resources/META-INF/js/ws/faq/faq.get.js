ec92cda8cad9567a28281b3666f027f74b5b4742
marco.spasiano <marco.spasiano@cnr.it>
2019-09-19 18:11:04 +0200

    Changed implementation of faqs

--- cool-jconon-webapp-resources/src/main/resources/META-INF/js/ws/faq/faq.get.js
+++ cool-jconon-webapp-resources/src/main/resources/META-INF/js/ws/faq/faq.get.js
@@ -1,28 +1,36 @@
-require(['jquery', 'cnr/cnr.url', 'header'], function ($, URL) {
+require(['jquery', 'cnr/cnr.url', 'json!cache', 'header'], function ($, URL, cache) {
   "use strict";
-
-  var categories = {
-    dipendenti: 'Selezioni per dipendenti CNR',
-    tutti: 'Tutti i Bandi'
-  };
-
-  $.each(categories, function (id, name) {
-
-    var target = $('#' + id);
-
-    URL.Data.frontOffice.document({
-      placeholder: {
-        'type_document': 'faq'
-      },
-      data: {
-        typeBando: name
-      }
-    }).done(function (data) {
-      $.each(data.docs, function (index, el) {
-        var html = '<h4>' + el.question + '</h4>' + el.answer;
-        target.append(html);
-      });
+    var cnraffix = $('ul.cnraffix'), question = $('#field'), target;
+    function changeActiveState(btn) {
+      btn.parents('ul').find('.active').removeClass('active');
+      btn.parent('li').addClass('active');
+    }
+    $.each($.merge([{'key':'Tutti i Bandi', 'title' : 'Tutti i Bandi'}], cache.jsonlistCallType), function (id, el) {
+        URL.Data.frontOffice.document({
+          placeholder: {
+            'type_document': 'faq'
+          },
+          data: {
+            typeBando: el.key
+          }
+        }).done(function (data) {
+          if (data.docs.length > 0) {
+            cnraffix.append($('<li class="' + (id == 0 ? 'active' : '') + '">').append(
+                $('<a href="#'+ id +'">' + el.title + '</a>').click(function (eventObject) {
+                    changeActiveState($(eventObject.target));
+                })
+            ));
+            var section = $('<section id="' + id +'" class="spacer">');
+            target = $('<div class="well">');
+            target.append($('<h1>').append(el.title).append($('<hr>')));
+            $.each(data.docs, function (index, el) {
+                var html = '<h4>' + el.question + '</h4>' + el.answer;
+                target.append(html);
+            });
+            section.append(target);
+            section.append($('<p><a href="#"><i class="icon-arrow-up"></i> torna all\'inizio</a></p>'));
+            question.append(section);
+          }
+        });
     });
-  });
-
 });
