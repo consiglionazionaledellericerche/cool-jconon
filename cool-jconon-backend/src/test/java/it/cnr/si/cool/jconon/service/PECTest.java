518008164f76da3d66de198c9b01fadc7ee567f3
marco.spasiano <marco.spasiano@cnr.it>
2019-10-25 15:53:00 +0200

    Add test for PEC

--- cool-jconon-backend/src/test/java/it/cnr/si/cool/jconon/service/PECTest.java
+++ cool-jconon-backend/src/test/java/it/cnr/si/cool/jconon/service/PECTest.java
@@ -17,8 +17,13 @@
 
 package it.cnr.si.cool.jconon.service;
 
+import it.cnr.cool.util.MimeTypes;
 import it.cnr.si.cool.jconon.configuration.PECConfiguration;
 import it.cnr.si.cool.jconon.dto.VerificaPECTask;
+import it.cnr.si.cool.jconon.util.SimplePECMail;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.EmailException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,9 +36,15 @@ import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Store;
 import javax.mail.URLName;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.mail.search.SearchTerm;
 import javax.mail.search.SubjectTerm;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.Properties;
 
@@ -44,12 +55,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 public class PECTest {
+    public static final String NAME_ATTACHMENTS = "allegato.pdf";
+
     @Value("${pec.username}")
     private String pecUsername;
-
     @Value("${pec.password}")
     private String pecPassword;
-
+    @Value("${pec.to}")
+    private String pecTo;
     @Autowired
     private PECConfiguration pecConfiguration;
 
@@ -95,6 +108,31 @@ public class PECTest {
 
                 }
             });
+        }
+    }
+
+    @Test
+    public void sendPEC() {
+        SimplePECMail simplePECMail = new SimplePECMail(pecUsername, pecPassword);
+        simplePECMail.setHostName(pecConfiguration.getHostSmtp());
+        simplePECMail.setSubject("TEST PEC");
+        String content = "Con riferimento alla Sua domanda di partecipazione al concorso indicato in oggetto, si invia in allegato la relativa esclusione.<br>";
+        content += "Distinti saluti.<br/><br/><br/><hr/>";
+        content += "<b>Questo messaggio e' stato generato da un sistema automatico. Si prega di non rispondere.</b><br/><br/>";
+        try {
+            simplePECMail.setFrom(pecUsername);
+            simplePECMail.setReplyTo(Collections.singleton(new InternetAddress("undisclosed-recipients")));
+            simplePECMail.setTo(Collections.singleton(new InternetAddress(pecTo)));
+            final byte[] bytes = IOUtils.toByteArray(getClass().getResourceAsStream(
+                    "/" + NAME_ATTACHMENTS));
+            simplePECMail.attach(new ByteArrayDataSource(new ByteArrayInputStream(content.getBytes()),
+                    "text/html"), "", "", EmailAttachment.INLINE);
+            simplePECMail.attach(new ByteArrayDataSource(new ByteArrayInputStream(bytes),
+                    MimeTypes.PDF.mimetype()), NAME_ATTACHMENTS, NAME_ATTACHMENTS);
+            final String send = simplePECMail.send();
+            assertNotNull(send);
+        } catch (EmailException | AddressException | IOException e) {
+            assertNotNull(e);
         }
     }
 }
