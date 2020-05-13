48314ccb355862e04ea809c06485bcccb3b13881
marco.spasiano <marco.spasiano@cnr.it>
2020-05-13 08:52:33 +0200

    Add CorsFilter to SpringBootApplication

--- cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/CoolJcononApplication.java
+++ cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/CoolJcononApplication.java
@@ -24,6 +24,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
 
 
@@ -37,10 +40,27 @@ public class CoolJcononApplication {
 	public UrlRewriteFilter getUrlRewriteFilter() {
 		UrlRewriteFilter urlRewriteFilter = new UrlRewriteFilter();
 		return urlRewriteFilter;
-
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoolJcononApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
