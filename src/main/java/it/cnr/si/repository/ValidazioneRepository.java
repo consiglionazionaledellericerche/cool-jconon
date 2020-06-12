9d4b1a36213ff1c1f5f11ab5bdb66aabf21d04bc
valerio.diego <valerio.diego@cnr.it>
2020-06-12 16:15:54 +0200

    Validazione vedono solo il proprio istituto

--- src/main/java/it/cnr/si/repository/ValidazioneRepository.java
+++ src/main/java/it/cnr/si/repository/ValidazioneRepository.java
@@ -1,7 +1,10 @@
 package it.cnr.si.repository;
 
 import it.cnr.si.domain.Validazione;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -12,4 +15,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ValidazioneRepository extends JpaRepository<Validazione, Long> {
 
+    @Query("SELECT va FROM Validazione va where va.veicolo.istituto like :istituto")
+    public Page<Validazione> findByIstituto(@Param("istituto") String istituto, Pageable pageable);
 }
