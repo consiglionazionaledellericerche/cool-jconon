6bc7059aaedb20edff52170d21e1fd8c90630219
mspasiano <marco.spasiano@cnr.it>
2020-09-25 08:20:36 +0200

    Change docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 #      You should have received a copy of the GNU Affero General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import sphinx_material
+import docs_italia_theme
 
 # Register the theme as an extension to generate a sitemap.xml
 # extensions.append('sphinx_material')
@@ -22,8 +22,8 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 
-project = 'Selezioni on-line'
-release = '4.4.23'
+project = 'Selezioni online'
+release = '1.0.0'
 author = u'Consiglio Nazionale delle Ricerche'
 
 show_authors = True
@@ -36,19 +36,17 @@ latex_elements = {
     'extraclassoptions': 'openany,oneside'
 }
 epub_basename = u'Manuale-Selezioni-online'
-# Choose the material theme
-html_theme = 'sphinx_material'
-#html_theme = 'sphinx_materialdesign_theme'
-# Get the them path
-html_theme_path = sphinx_material.html_theme_path()
-# Register the required helpers for the html context
-html_context = sphinx_material.get_html_context()
+
+html_theme = "docs_italia_theme"
+html_theme_path = [docs_italia_theme.get_html_theme_path()]
+
 copyright = "2020 Consiglio Nazionale delle Ricerche"
+html_title = "Selezioni online"
 html_show_sourcelink = False
 html_favicon = "favicon.ico"
 html_logo = "logo.png"
 latex_logo = 'logo_blue.png'
-html_title = "Home"
+html_baseurl = 'docs'
 smartquotes = False
 language = "it"
 # The master toctree document.
@@ -68,6 +66,7 @@ html_css_files = [
     'css/material.css'
 ]
 extensions = [
+    'docs_italia_theme',
     'sphinxcontrib.images',
     "sphinx.ext.autodoc",
     "numpydoc",
@@ -81,16 +80,4 @@ extensions = [
     "sphinx_markdown_tables",
     'sphinx.ext.githubpages'
 ]
-html_theme_options = {
-    'base_url': 'https://consiglionazionaledellericerche.github.io/cool-jconon',
-    'repo_url': 'https://github.com/consiglionazionaledellericerche/cool-jconon/',
-    'repo_name': 'consiglionazionaledellericerche/cool-jconon',
-    'nav_title': 'Selezioni online',
-    'html_minify': True,
-    'css_minify': True,
-    'version_dropdown': False,
-    'globaltoc_depth': 5,
-    # Set the color and the accent color
-    'color_primary': 'indingo',
-    'color_accent': 'cyan'
-}
+
