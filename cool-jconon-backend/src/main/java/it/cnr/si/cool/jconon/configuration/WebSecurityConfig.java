956ba42d1113b6948d59364772453358c0506d6f
marco.spasiano <marco.spasiano@cnr.it>
2020-10-29 08:23:12 +0100

    Add ROLE on actuator endpoint

--- cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/configuration/WebSecurityConfig.java
+++ cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/configuration/WebSecurityConfig.java
@@ -1,10 +1,25 @@
 package it.cnr.si.cool.jconon.configuration;
 
+import it.cnr.cool.cmis.service.LoginException;
+import it.cnr.cool.security.CMISAuthenticatorFactory;
+import it.cnr.cool.security.service.impl.alfresco.CMISGroup;
+import it.cnr.cool.security.service.impl.alfresco.CMISUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 @Configuration
@@ -12,13 +27,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${security.enable-csrf}")
     private boolean csrfEnabled;
+    @Autowired
+    private CMISAuthenticatorFactory cmisAuthenticatorFactory;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+
                 .authorizeRequests()
+                .antMatchers("/actuator/health")
+                .permitAll()
+                .antMatchers("/actuator/*")
+                .hasRole("GROUP_ALFRESCO_ADMINISTRATORS")
                 .antMatchers("/**")
                 .permitAll()
+                .and()
+                .httpBasic()
                 .and()
                 .csrf()
                 .ignoringAntMatchers("/spid/**", "/spid/", "/openapi/**")
@@ -27,9 +51,81 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .xssProtection()
                 .and()
                 .frameOptions();
-        if(!csrfEnabled)
-        {
+        if (!csrfEnabled) {
             http.csrf().disable();
+        }
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(new CustomAuthenticationProvider());
+    }
+
+    class CustomAuthenticationProvider implements AuthenticationProvider {
+
+        @Override
+        public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+            String name = authentication.getName();
+            String password = authentication.getCredentials().toString();
+            try {
+                CMISUser cmisUserFromSession = cmisAuthenticatorFactory.getCMISUser(
+                        cmisAuthenticatorFactory.getTicket(name, password)
+                );
+                return new Authentication() {
+                    @Override
+                    public Collection<? extends GrantedAuthority> getAuthorities() {
+                        return cmisUserFromSession
+                                .getGroups()
+                                .stream()
+                                .map(CMISGroup::getGroup_name)
+                                .map(s -> "ROLE_".concat(s))
+                                .map(s -> new GrantedAuthority() {
+                                    @Override
+                                    public String getAuthority() {
+                                        return s;
+                                    }
+                                })
+                                .collect(Collectors.toList());
+                    }
+
+                    @Override
+                    public Object getCredentials() {
+                        return null;
+                    }
+
+                    @Override
+                    public Object getDetails() {
+                        return cmisUserFromSession;
+                    }
+
+                    @Override
+                    public Object getPrincipal() {
+                        return cmisUserFromSession.getUserName();
+                    }
+
+                    @Override
+                    public boolean isAuthenticated() {
+                        return !cmisUserFromSession.isGuest();
+                    }
+
+                    @Override
+                    public void setAuthenticated(boolean b) throws IllegalArgumentException {
+
+                    }
+
+                    @Override
+                    public String getName() {
+                        return cmisUserFromSession.getUserName();
+                    }
+                };
+            } catch (LoginException e) {
+                throw new BadCredentialsException("Bad Credentials", e);
+            }
+        }
+
+        @Override
+        public boolean supports(Class<?> authentication) {
+            return authentication.equals(UsernamePasswordAuthenticationToken.class);
         }
     }
 }
--- cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/rest/openapi/controllers/IndexController.java
+++ cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/rest/openapi/controllers/IndexController.java
@@ -1,17 +1,41 @@
 package it.cnr.si.cool.jconon.rest.openapi.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 @Controller
-public class IndexController implements ErrorController{
+public class IndexController implements ErrorController {
     private final static String PATH = "/error";
-    @Override
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
     @RequestMapping(PATH)
     @ResponseBody
+    public String handleError(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+        String format = String.format("<html><body><h1>Error Page</h1><div>Status code: <b>%s</b></div>"
+                        + "<div>Exception Message: <b>%s</b></div><body></html>",
+                statusCode, exception == null ? "N/A" : exception.getMessage());
+        LOGGER.error(
+                "ERROR Page Controller Status:{} Message:{}",
+                statusCode,
+                Optional.ofNullable(exception)
+                    .flatMap(e -> Optional.ofNullable(e.getMessage()))
+                    .orElse("N/A")
+        );
+        return format;
+    }
+
+    @Override
     public String getErrorPath() {
-        return "No Mapping Found";
+        return PATH;
     }
 
 }
