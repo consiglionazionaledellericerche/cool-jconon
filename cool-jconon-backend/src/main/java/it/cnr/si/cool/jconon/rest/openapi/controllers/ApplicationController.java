7ede7087b4e7120aac943924606c0c89d91658dd
marco.spasiano <marco.spasiano@cnr.it>
2021-01-15 18:39:11 +0100

    Add save on ApplicationController

--- cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/rest/openapi/controllers/ApplicationController.java
+++ cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/rest/openapi/controllers/ApplicationController.java
@@ -1,24 +1,29 @@
 package it.cnr.si.cool.jconon.rest.openapi.controllers;
 
 import it.cnr.cool.cmis.service.CMISService;
+import it.cnr.cool.cmis.service.NodeMetadataService;
+import it.cnr.cool.util.CMISUtil;
 import it.cnr.cool.util.Pair;
 import it.cnr.si.cool.jconon.rest.openapi.utils.ApiRoutes;
 import it.cnr.si.cool.jconon.service.application.ApplicationService;
 import it.cnr.si.cool.jconon.util.FilterType;
+import org.apache.chemistry.opencmis.client.api.Folder;
 import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.commons.PropertyIds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+import java.text.ParseException;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(ApiRoutes.V1_APPLICATION)
@@ -28,6 +33,8 @@ public class ApplicationController {
     private CMISService cmisService;
     @Autowired
     private ApplicationService applicationService;
+    @Autowired
+    private NodeMetadataService nodeMetadataService;
 
     @GetMapping
     public ResponseEntity<Map<String, Object>> list(HttpServletRequest req,
@@ -102,5 +109,25 @@ public class ApplicationController {
         Session session = cmisService.getCurrentCMISSession(req);
         return ResponseEntity.ok().body(
                 applicationService.findApplicationState(session, user));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Map<String, ?>> saveApplication(HttpServletRequest req, @RequestBody Map<String, ?> prop) throws ParseException {
+        Session session = cmisService.getCurrentCMISSession(req);
+
+        Map<String, Object> properties = nodeMetadataService
+                .populateMetadataType(session, prop, req);
+        return ResponseEntity.ok(
+                CMISUtil.convertToProperties(
+                        applicationService.save(
+                                session,
+                                null,
+                                req.getLocale(),
+                                cmisService.getCMISUserFromSession(req).getId(),
+                                properties,
+                                Collections.emptyMap()//aspectProperties
+                        )
+                )
+        );
     }
 }
