a04112a3e7bdd2a27363230b385d31a7cf568fa9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-20 19:54:36 +0000

    Deploy consiglionazionaledellericerche/cool-jconon to github.com/consiglionazionaledellericerche/cool-jconon.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.5.31',
+    VERSION: '4.5.32',
     LANGUAGE: 'it',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
