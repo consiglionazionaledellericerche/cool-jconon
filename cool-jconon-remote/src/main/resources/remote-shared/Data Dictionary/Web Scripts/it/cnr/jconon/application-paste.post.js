33c33e6c9a7b00930c4bb856769928b3d7b61270
marco.spasiano <marco.spasiano@cnr.it>
2021-08-09 19:11:57 +0200

    Remove aspect protocol on application paste

--- cool-jconon-remote/src/main/resources/remote-shared/Data Dictionary/Web Scripts/it/cnr/jconon/application-paste.post.js
+++ cool-jconon-remote/src/main/resources/remote-shared/Data Dictionary/Web Scripts/it/cnr/jconon/application-paste.post.js
@@ -61,6 +61,7 @@ function main() {
   applicationTarget.getProperties()["jconon_application:stato_domanda"] = "P";
   applicationTarget.getProperties()["jconon_application:data_domanda"] = null;
   applicationTarget.getProperties()["jconon_application:esclusione_rinuncia"] = null;
+  applicationTarget.removeAspect('jconon_protocollo:common');
   applicationTarget.save();
   applicationTarget.removeAspect('jconon_application:aspect_punteggi');
   cnrutils.getBean("permissionService").deletePermissions(applicationTarget.getNodeRef());
