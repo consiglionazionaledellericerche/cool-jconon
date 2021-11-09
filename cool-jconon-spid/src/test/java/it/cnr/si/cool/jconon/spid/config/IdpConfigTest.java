d4ecad6ee0cbf5e1acf5d2966db163222b149090
marco.spasiano <marco.spasiano@cnr.it>
2021-11-09 10:41:11 +0100

    FIX test

--- cool-jconon-spid/src/test/java/it/cnr/si/cool/jconon/spid/config/IdpConfigTest.java
+++ cool-jconon-spid/src/test/java/it/cnr/si/cool/jconon/spid/config/IdpConfigTest.java
@@ -24,19 +24,15 @@ import org.joda.time.DateTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.opensaml.common.SAMLException;
-import org.opensaml.common.SAMLVersion;
 import org.opensaml.saml2.core.AuthnRequest;
-import org.opensaml.saml2.core.impl.AuthnRequestBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Base64;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -65,7 +61,7 @@ public class IdpConfigTest {
         final Optional<String> first = spidIntegrationService.getListIdp().keySet().stream().findFirst();
         TimeUnit.SECONDS.sleep(1);
         final Optional<String> second = spidIntegrationService.getListIdp().keySet().stream().findFirst();
-        assertEquals(Boolean.FALSE,first.equals(second));
+        assertEquals(Boolean.FALSE, first.equals(second));
     }
 
     @Test
@@ -84,4 +80,21 @@ public class IdpConfigTest {
 
     }
 
+    @Test
+    public void validateResponseAggregatorCIE() throws IOException, AuthenticationException, SAMLException {
+        idpConfiguration.getSpidProperties().getAggregator().setIssuer("https://login.regione.umbria.it/gw/metadata");
+        idpConfiguration.getSpidProperties().getAttribute().setName("nome");
+        final AuthnRequest authnRequest =
+                spidIntegrationService.buildAuthenticationRequest(
+                        "",
+                        Optional.of("_ddbaf4fc-65f0-4a05-9daa-815ce98b9f82"),
+                        Optional.of(DateTime.parse("2021-11-08T07:41:00.490Z"))
+                );
+
+        final String response = Base64.getEncoder().encodeToString(
+                IOUtils.toByteArray(this.getClass().getResourceAsStream("/aggregator-cie-response.xml"))
+        );
+        assertNotNull(spidIntegrationService.idpResponse(response));
+
+    }
 }
