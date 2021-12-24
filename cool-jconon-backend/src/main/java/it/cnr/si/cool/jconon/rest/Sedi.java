4fb2930a42dffd8dc4f78906263a041c60bbadf7
marco.spasiano <marco.spasiano@cnr.it>
2021-12-24 12:05:18 +0100

    Add Static sedi

--- cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/rest/Sedi.java
+++ cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/rest/Sedi.java
@@ -16,12 +16,18 @@
 
 package it.cnr.si.cool.jconon.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.cnr.cool.cmis.service.CMISService;
 import it.cnr.si.cool.jconon.dto.SiperSede;
 import it.cnr.si.cool.jconon.exception.SiperException;
 import it.cnr.si.cool.jconon.service.SiperService;
+import org.apache.chemistry.opencmis.client.api.Document;
+import org.apache.chemistry.opencmis.commons.data.ContentStream;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,8 +38,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Collection;
-import java.util.Optional;
+import java.io.IOException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Path("sedi")
@@ -45,6 +51,11 @@ public class Sedi {
 	@Autowired(required = false)
 	private SiperService siperService;
 
+	@Autowired
+	private CMISService cmisService;
+
+	@Value("${siper.static.sedi}")
+	private Boolean isStaticSedi;
 	@GET
 	public Response getSedi(@Context HttpServletRequest req, @QueryParam("attive") Boolean attive) {
 		if (Optional.ofNullable(siperService).isPresent()) {
@@ -63,16 +74,55 @@ public class Sedi {
 					.ok(sedi)
 					.build();
 		}
+		if (isStaticSedi) {
+			try {
+				return Response
+						.ok(getStaticSedi())
+						.build();
+			} catch (CmisObjectNotFoundException _ex) {
+				LOGGER.warn("sedi.json not found");
+			}
+		}
 		return Response.ok().build();
 	}
 
 	@GET
 	@Path("gestori")
 	public Response getSede(@Context HttpServletRequest req, @QueryParam("sedeId") String sedeId) {
-		Optional<SiperSede> siperSede = Optional.ofNullable(siperService).map(s -> s.cacheableSiperSede(sedeId)).orElse(Optional.empty());
+		Optional<SiperSede> siperSede = Optional.empty();
+		if (isStaticSedi) {
+			siperSede = getStaticSedi()
+					.stream()
+					.filter(siperSede1 -> siperSede1.getSedeId().equals(sedeId))
+					.findAny();
+		} else {
+			siperSede = Optional.ofNullable(siperService)
+					.map(s -> s.cacheableSiperSede(sedeId))
+					.orElse(Optional.empty());
+
+		}
 		return Response
 				.ok(siperSede.orElseThrow(() -> new  SiperException("sede " + sedeId + " not found")))
 				.build();
 	}
 
+	private List<SiperSede> getStaticSedi() {
+		List<SiperSede> siperSedes = new ArrayList<SiperSede>();
+		ObjectMapper objectMapper = new ObjectMapper();
+		Optional.ofNullable(cmisService
+						.createAdminSession()
+						.getObjectByPath(SiperService.SEDI_JSON))
+				.filter(Document.class::isInstance)
+				.map(Document.class::cast)
+				.map(Document::getContentStream)
+				.map(ContentStream::getStream)
+				.ifPresent(inputStream1 -> {
+					try {
+						siperSedes.addAll(Arrays.asList(objectMapper.readValue(inputStream1, SiperSede[].class)));
+					} catch (IOException e) {
+						LOGGER.error("Cannot read document of sedi.json",e);
+					}
+				});
+		return siperSedes;
+	}
 }
