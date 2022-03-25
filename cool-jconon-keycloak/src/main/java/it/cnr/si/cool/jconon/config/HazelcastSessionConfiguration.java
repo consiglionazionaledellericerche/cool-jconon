f63d128cc506905a696de577a319499f343e6c39
marco.spasiano <marco.spasiano@cnr.it>
2022-03-25 10:15:21 +0100

    Add hazelcast session and remove cool-jconon-spid from webapp

---
+++ cool-jconon-keycloak/src/main/java/it/cnr/si/cool/jconon/config/HazelcastSessionConfiguration.java
@@ -0,0 +1,12 @@
+package it.cnr.si.cool.jconon.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.session.hazelcast.config.annotation.web.http.HazelcastHttpSessionConfiguration;
+
+@Configuration(proxyBeanMethods = false)
+@Profile("keycloak")
+public class HazelcastSessionConfiguration extends HazelcastHttpSessionConfiguration {
+
+
+}
