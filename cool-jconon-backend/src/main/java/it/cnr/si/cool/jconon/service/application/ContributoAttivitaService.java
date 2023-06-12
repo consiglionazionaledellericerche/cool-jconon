91715e2c4892d434b05213f57d3d92567b1e48aa
marco.spasiano <marco.spasiano@cnr.it>
2023-06-12 09:44:26 +0200

    Added two new sections for declarations

--- cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/service/application/ContributoAttivitaService.java
+++ cool-jconon-backend/src/main/java/it/cnr/si/cool/jconon/service/application/ContributoAttivitaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 @Service
 public class ContributoAttivitaService implements ApplicationValidateSend{
@@ -15,18 +16,27 @@ public class ContributoAttivitaService implements ApplicationValidateSend{
 
     @Override
     public void validate(Folder call, Folder application) {
-        final Integer maxlength =
-                Optional.ofNullable(application.<String>getPropertyValue("jconon_application:contributo_attivita_dimensione1"))
-                    .map(s -> s.length())
-                    .orElse(0) +
-                Optional.ofNullable(application.<String>getPropertyValue("jconon_application:contributo_attivita_dimensione2"))
-                    .map(s -> s.length())
-                    .orElse(0) +
-                Optional.ofNullable(application.<String>getPropertyValue("jconon_application:contributo_attivita_dimensione3"))
-                    .map(s -> s.length())
-                    .orElse(0);
-        if (maxlength > MAXLENGTH) {
-            throw new ClientMessageException("Il numero dei caratteri [" + maxlength + "] supera la dimensione massima [" + MAXLENGTH + "]");
+
+        final Integer maxLengthRicercatore = IntStream.range(1, 4)
+                .map(i -> applicationValue(application, "jconon_application:contributo_attivita_dimensione" + i))
+                .reduce(0, (a, b) -> a + b);
+
+        if (maxLengthRicercatore > MAXLENGTH) {
+            throw new ClientMessageException("Il numero dei caratteri [" + maxLengthRicercatore + "] supera la dimensione massima [" + MAXLENGTH + "]");
         }
+        final Integer maxLengthTecnologo = IntStream.range(1, 11)
+                .map(i -> applicationValue(application, "jconon_application:tecnologo_settore_ambiti_" + i))
+                .reduce(0, (a, b) -> a + b);
+
+        if (maxLengthRicercatore > MAXLENGTH) {
+            throw new ClientMessageException("Il numero dei caratteri [" + maxLengthTecnologo + "] supera la dimensione massima [" + MAXLENGTH + "]");
+        }
+
+    }
+
+    private Integer applicationValue(Folder application, String property) {
+        return Optional.ofNullable(application.<String>getPropertyValue(property))
+                .map(s -> s.length())
+                .orElse(0);
     }
 }
