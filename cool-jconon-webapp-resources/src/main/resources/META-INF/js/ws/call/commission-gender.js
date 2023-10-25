27418dd50dd6c0a3614f79426664654b2eecbd0f
marco.spasiano <marco.spasiano@cnr.it>
2023-10-25 16:23:23 +0200

    Management of the video for gender equality by the commissioners, the use of the video takes place through the PeerTube API

--- cool-jconon-webapp-resources/src/main/resources/META-INF/js/ws/call/commission-gender.js
+++ cool-jconon-webapp-resources/src/main/resources/META-INF/js/ws/call/commission-gender.js
@@ -1,11 +1,9 @@
 require(['jquery', 'header', 'cnr/cnr', 'cnr/cnr.url', 'cnr/cnr.ui', 'json!cache', 'i18n', 'cnr/cnr.ui.select', 'cnr/cnr.application', 'cnr/cnr.jconon', 'cnr/cnr.call', 'json!common'],
     function ($, header, CNR, URL, UI, cache, i18n, select, Application, jconon, Call, common) {
   "use strict";
-    const PeerTubePlayer = window['PeerTubePlayer'];
-
-    let player = new PeerTubePlayer(window.frames["video"]);
-
+    var playbackState;
     const status = function(event) {
+        playbackState = event.playbackState;
         if (event.playbackState == 'ended' && event.position == event.duration) {
             player.removeEventListener('playbackStatusUpdate', status);
             URL.Data.proxy.metadataNode({
@@ -29,8 +27,29 @@ require(['jquery', 'header', 'cnr/cnr', 'cnr/cnr.url', 'cnr/cnr.ui', 'json!cache
     }
 
     const initialize = async () => {
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(() => resolve("fatto!"), 1000)
+        });
+        let result = await promise;
+
+        const PeerTubePlayer = window['PeerTubePlayer'];
+
+        let player = new PeerTubePlayer(window.frames["video"]);
         await player.ready; // wait for the player to be ready
         console.log({player});
+        $('#play').toggle();
+        $('#pause').toggle();
+        $('#play').on('click', function () {
+            player.play();
+            setTimeout(() => {
+                if (playbackState && playbackState == 'unstarted') {
+                    UI.alert(i18n['message.video.gender.browser']);
+                }
+            }, 1000);
+        });
+        $('#pause').on('click', function () {
+            player.pause();
+        });
         player.addEventListener('playbackStatusUpdate', status);
     }
     initialize();
--- cool-jconon-webapp-resources/src/main/resources/pages/call/commission-gender.get.html.ftl
+++ cool-jconon-webapp-resources/src/main/resources/pages/call/commission-gender.get.html.ftl
@@ -1,7 +1,11 @@
 <div class="container">
     <div class="row-fluid">
         <div class="container-fluid">
-            <iframe id="video" src="${videoGenderURL}" class="span12" style="height:90vh">
+            <iframe id="video" src="${videoGenderURL}" class="span12" style="height:80vh"></iframe>
+            <div class="btn-block">
+                <button class="btn btn-large span6 btn-primary hide" id="play"><i class="icon-play icon-white"></i> Play</button>
+                <button class="btn btn-large span6 btn-danger hide" id="pause"><i class="icon-pause icon-white"></i> Pause</button>
+            </div>
         </div>
     </div>
 </div>
