2f75fde941686275fb1f421d5e6a033324b76145
mspasiano <mspasiano@users.noreply.github.com>
2026-04-16 13:09:06 +0000

    deploy: 6a807780386341d9f03c4d4939833ad1d14ea4a5

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '5.10.3',
+    LANGUAGE: 'it',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
